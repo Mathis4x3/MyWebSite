2556e4bd0c8585495db1a545ca3b03bea5f6cc59
mathis ourliac <mathis OURLIAC>
2024-09-21 21:40:02 +0200

    navbar de qualité, a changer je pense

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let navburger = document.getElementById("navbarB");
+let burger = document.getElementById("burger");
+
+burger.addEventListener("click", function (){
+    navburger.classList.toggle("clicked");
+})
